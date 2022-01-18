88cde03619174c6bfdd9e48c6e02e8e63ab637ed
renyuanxin <renyuanxin@ttjianbao.com>
2022-01-18 17:59:13 +0800

    first commit

---
+++ src/main/java/com/design/doubleDimple/DesignApplication.java
@@ -0,0 +1,19 @@
+package com.design.doubleDimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName com.design.doubleDimple.DesignApplication
+ * @Description create by renyuanxin at 2022/1/18 5:52 下午
+ */
+@SpringBootApplication
+public class DesignApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignApplication.class,args);
+        System.out.println("启动成功!!!!!");
+    }
+
+}
